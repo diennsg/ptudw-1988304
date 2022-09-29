16b535d29cccaea14d27d3157d74f58d74e706b0
diennsg <43106538+diennsg@users.noreply.github.com>
2022-09-30 00:12:56 +0700

    Update index.js
    loai bo aroma

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ app.get('/', (req, res)=>{
     res.send('Hello World');
 });
 
-app.get('/aroma', (req,res)=>{
-    res.send('This is arome web page !');
-});
-
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), ()=>{
     console.log(`server is running on port ${app.get('port')}`);
