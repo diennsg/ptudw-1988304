dc6e999f3bbd8cf65e7ecf686344dba77e2d0be4
diennsg <diennsg@gmail.com>
2022-09-29 23:59:57 +0700

    them /aroma

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res)=>{
     res.send('Hello World');
 });
 
+app.get('/aroma', (req,res)=>{
+    res.send('This is arome web page !');
+});
+
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), ()=>{
     console.log(`server is running on port ${app.get('port')}`);
