632b57979bc9ed55dbcb76af007b770849a499c5
diennsg <diennsg@gmail.com>
2022-09-30 19:18:54 +0700

    add aroma

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
+// static: /public: (.html)
+app.use(express.static(__dirname  + '/public'));
+
+// express-handlebars
+var expHbs = require('express-handlebars');
+var hbs = expHbs.create({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+// render('blog')
+app.get('/:page', (req,res)=>{
+    var banners = {
+        blog: '-My Blog-',
+        cart: '-Shopping Cart-',
+        category: '-Shop Category-',
+        checkout: '-Product Checkout-',
+        confirmation: '-Order Confirmation-',
+        contact: '-Contact Us-',
+        login: '-Login / Register-',
+        register: '-Register-',
+        single_blog: '-Blog Detail-',
+        single_product: '-Shop Single-',
+        tracking_order: '-Order Tracking-',
+    } 
+    var page = req.params.page;
+    res.render(page, {banner: banners[page]});
+});
+
+// route goc
 app.get('/', (req, res)=>{
-    res.send('Hello World');
+    res.render('index');
 });
 
 app.set('port', process.env.PORT || 3000);
